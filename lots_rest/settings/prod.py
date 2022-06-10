e8f4f1a88e58e80c76a3c08ef02838fc611cbe71
Olivier Perceval <olivier@percil.solutions>
2022-06-10 09:29:48 +0200

    updating Dockerfile. again.

--- lots_rest/settings/prod.py
+++ lots_rest/settings/prod.py
@@ -1,4 +1,8 @@
+import logging
+
 from .base import *
+
+logger = logging.getLogger('prod')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -6,7 +10,7 @@ DEBUG = False
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
-print(SECRET_KEY)
+logger.warning(f'secret_key={SECRET_KEY}')
 
 # SECURITY WARNING: define the correct hosts in production!
 CSRF_TRUSTED_ORIGINS = ['https://lots.percil.be']
