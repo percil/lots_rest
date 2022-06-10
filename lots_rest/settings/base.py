c74981e059ff20cdc8d7637164e45b4973468ab8
Olivier Perceval <olivier@percil.solutions>
2022-06-10 10:03:08 +0200

    fixing static files. again.

--- lots_rest/settings/base.py
+++ lots_rest/settings/base.py
@@ -118,9 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Static files for production
-STATICFILES_DIR = (os.path.join(BASE_DIR, 'staticfiles'),)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
